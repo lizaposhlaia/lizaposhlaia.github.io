e21b32f2a035816e29117f52dc5879b86fd4b670
lizaposhlaia <76399398+lizaposhlaia@users.noreply.github.com>
2022-06-13 20:13:54 +0700

    Update script.js

--- forum/js/script.js
+++ forum/js/script.js
@@ -1,1 +1,68 @@
-if(null!==document.querySelector("#bth-modal")){const t=document.querySelectorAll("#bth-modal"),a=document.querySelectorAll("#body-modal"),e=(document.querySelector("body"),document.querySelectorAll("#body-close-modal"));function checkActivModalBth(a){t.forEach(t=>{t.attributes.data.value==a&&(t.classList.contains("modal-active")?t.classList.remove("modal-active"):t.classList.add("modal-active"))})}function openModal(t){t.classList.add("modal-active")}function closeModal(t){t.classList.remove("modal-active")}t.forEach(t=>{t.addEventListener("click",(function(t){a.forEach(a=>{a.attributes.data.value==t.target.attributes.data.value&&(a.classList.contains("modal-active")?(closeModal(a),checkActivModalBth(t.target.attributes.data.value)):(openModal(a),checkActivModalBth(t.target.attributes.data.value)))})}))}),e.forEach(t=>{t.addEventListener("click",(function(t){a.forEach(a=>{a.attributes.data.value==t.target.attributes.data.value&&a.classList.contains("modal-active")&&(closeModal(a),checkActivModalBth(t.target.attributes.data.value))})}))})}
+if (document.querySelector('#bth-modal') !== null) {
+
+    const modalBth = document.querySelectorAll('#bth-modal');
+    const modalBody = document.querySelectorAll('#body-modal');
+    const body = document.querySelector('body');
+    const modalBodyClose = document.querySelectorAll('#body-close-modal');
+
+   //console.log(modalBody[0].attributes.data.value);
+
+    //console.log(document.querySelectorAll('*[data]'));
+
+    modalBth.forEach(elModalBth => {
+        elModalBth.addEventListener("click", function (e) {
+            //console.log(e.target.attributes.data.value);
+
+            modalBody.forEach(elModalBody => {
+                if (elModalBody.attributes.data.value == e.target.attributes.data.value) {
+                    if (elModalBody.classList.contains('modal-active')) {
+                        closeModal(elModalBody);
+                        checkActivModalBth(e.target.attributes.data.value);
+                    }
+                    else {
+                        openModal(elModalBody);
+                        checkActivModalBth(e.target.attributes.data.value);
+                    }
+                }
+            })
+        })
+    })
+    
+    modalBodyClose.forEach(modalBodyClose => {
+        modalBodyClose.addEventListener("click", function (e) {
+            //console.log(e.target.attributes.data.value);
+
+            modalBody.forEach(modalBody => {
+                if (modalBody.attributes.data.value == e.target.attributes.data.value) {
+                    if (modalBody.classList.contains('modal-active')) {
+                        closeModal(modalBody);
+                        checkActivModalBth(e.target.attributes.data.value);
+                    }
+                }
+            })
+        })
+    })
+
+    function checkActivModalBth(attr) {
+        modalBth.forEach(el => {
+            if (el.attributes.data.value == attr) {
+                if (el.classList.contains('modal-active')) {el.classList.remove('modal-active');}
+                else {el.classList.add('modal-active');}
+            }
+        });
+    }
+    
+    function openModal(modalBody) {
+        modalBody.classList.add('modal-active');
+        //body.classList.add('_lock');
+    }
+    
+    function closeModal(modalBody) {
+        modalBody.classList.remove('modal-active');
+        //body.classList.remove('_lock');
+    }
+    
+
+}
+
+
